ec38b0da0593a5f3bc0f737dddc86040e0a68107
mgruppi <mauricio.gruppi@gmail.com>
2019-05-29 17:33:38 -0400

    Setup dependencies and input.

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import networkx as nx
+
+
+def main():
+    # Read data in
+    input_path = "data/network.csv"
+    data = pd.read_csv(input_path)
+
+
+if __name__ == "__main__":
+    main()
