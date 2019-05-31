0c21a3d672d4105c1c8318b2b2f9c5c44bca0c77
mgruppi <mauricio.gruppi@gmail.com>
2019-05-31 15:03:36 -0400

    Add fit to text space.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 def main():
     # Read data in
     input_path = "data/news/edge_list.csv"
-    input_attributes = "data/news/test.txt"
+    input_attributes = "data/news/source_attributes.txt"
 
     g = nx.read_weighted_edgelist(input_path)
     attr = []
@@ -14,16 +14,19 @@ def main():
             line = line.split(",")
             attr.append(np.array(line[1:], dtype=float))
 
-    nt = nt2vec.NT2VEC(g, attr)
+    nt = nt2vec.NT2VEC(g, attr, dim=2)
 
     nt.precompute_attr_probabilities()
 
     for key in nt.d_graph:
         print(key, nt.d_graph[key][nt.PROB_KEY][nt.ATTR_PROB_KEY])
 
-    w = nt.generate_attr_walk()
-    for walk in w:
-        print(walk)
+    nt.generate_walks()
+    model = nt.fit()
+
+    r = model.wv.most_similar('0')
+    for v in r:
+        print(v)
 
 
 if __name__ == "__main__":
--- nt2vec.py
+++ nt2vec.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
+import gensim
 
 
 class NT2VEC:
@@ -9,7 +10,7 @@ class NT2VEC:
     ATTR_PROB_KEY = "attr"
     NETWORK_PROB_KEY = "network"
 
-    def __init__(self, graph, attr, dim=10, knn=10):
+    def __init__(self, graph, attr, dim=10, knn=10, workers=12):
         self.graph = graph  # networkX graph
         self.attr = attr  # node attributes
         self.dim = dim  # length of the output vectors
@@ -19,8 +20,11 @@ class NT2VEC:
         self.t = 0.5  # parameter that controls where to sample walks (0 is fully network, 1 is no network)
         self.num_walks = 10  # number of walks to sample
         self.walk_length = 30  # length of samples
+        self.walks = list()
         self.d_graph = defaultdict(dict)
         self.d_attr = defaultdict(dict)
+
+        self.workers = workers
 
     def precompute_nearest_neighbors(self):
 
@@ -30,7 +34,11 @@ class NT2VEC:
 
         # # normalize similarities into probability
         for i in range(len(similarities)):
-            similarities[i] = similarities[i] / sum(similarities[i])
+            sum_ = sum(similarities[i])
+            if sum_ != 0:
+                similarities[i] = similarities[i] / (sum(similarities[i]))
+
+
 
         return similarities, indices
 
@@ -49,6 +57,21 @@ class NT2VEC:
                 node = nn_indices[int(s)][i]
                 d_attr[s][self.PROB_KEY][node] = similarities[int(s)][i]
 
+    def generate_single_attr_walk(self, source):
+        d_attr = self.d_attr
+        walk = [source]
+
+        while len(walk) < self.walk_length:
+            destinations = list(d_attr[walk[-1]][self.PROB_KEY].keys())  # possible destinations
+            probabilities = list(d_attr[walk[-1]][self.PROB_KEY].values())  # list of probabilities
+            if not np.any(probabilities):
+                break
+            walk_to = np.random.choice(destinations, size=1, p=probabilities)[0]  # make a choice
+            walk.append(walk_to)
+        walk = list(map(str, walk))  # make sure walk contains only strings
+
+        return walk
+
     def generate_attr_walks(self):
         d_attr = self.d_attr
         walks = list()
@@ -61,12 +84,35 @@ class NT2VEC:
                 while len(walk) < self.walk_length:
                     destinations = list(d_attr[walk[-1]][self.PROB_KEY].keys())  # possible destinations
                     probabilities = list(d_attr[walk[-1]][self.PROB_KEY].values())  # list of probabilities
+                    if not np.any(probabilities):
+                        break
                     walk_to = np.random.choice(destinations, size=1, p=probabilities)[0]  # make a choice
                     walk.append(walk_to)
                     walk = list(map(str, walk))  # make sure walk contains only strings
                 walks.append(walk)
 
         return walks
+
+    def generate_walks(self):
+        for n_walk in range(self.num_walks):
+            for source in self.d_attr:
+                self.walks.append(self.generate_single_attr_walk(source))
+
+        return self.walks
+
+    def fit(self, **skip_gram_params):
+
+        if 'workers' not in skip_gram_params:
+            skip_gram_params['workers'] = self.workers
+
+        if 'size' not in skip_gram_params:
+            skip_gram_params["size"] = self.dim
+
+        if 'sg' not in skip_gram_params:
+            skip_gram_params["sg"] = 1  # 1 - use skip-gram; otherwise, use CBOW
+
+        return gensim.models.Word2Vec(self.walks, **skip_gram_params)
+
 
 
 
